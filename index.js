3c56614712c1dc391a9c18e9eadf7d4696db5c11
Daniel Lovell <danielalovell@gmail.com>
2019-03-31 16:38:25 -0700

    refactor for more generic names

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-function clicker(mountPoint, props) {
-  console.log("clicker " + props.id);
+function Clicker(mountPoint, props) {
+  console.log("Clicker" + props.id);
   const state = {
     count: 0
   };
 
   function setState(newState) {
     Object.assign(state, newState);
-    renderClicker();
+    render();
   }
 
   function handleClick() {
     setState({ count: state.count + 1 });
   }
 
-  function renderClicker() {
-    console.log("renderClicker" + props.id);
+  function render() {
+    console.log("clicker" + props.id);
     document.getElementById(mountPoint).innerHTML = `
-            <div>Clicker ${props.id}</div>
-            <div>Clicks: ${state.count}</div>
-            <button id=${props.id}>Click Me!</button>
-        `;
+        <div>Clicker ${props.id}</div>
+        <div>Clicks: ${state.count}</div>
+        <button id=${props.id}>Click Me!</button>
+    `;
     document.getElementById(props.id).addEventListener("click", handleClick);
   }
 
-  return renderClicker;
+  return render;
 }
 
-function app(mountPoint) {
-  console.log("app");
+function App(mountPoint) {
+  console.log("App");
 
-  const renderClicker1 = clicker("clicker1", { id: "1" });
-  const renderClicker2 = clicker("clicker2", { id: "2" });
+  const clickerA = Clicker("clickerA", { id: "A" });
+  const clickerB = Clicker("clickerB", { id: "B" });
 
-  function renderApp() {
-    console.log("renderApp");
+  function render() {
+    console.log("app");
     document.getElementById(mountPoint).innerHTML = `
-            <div>Daniel's Clickers</div>
-            <div id=clicker1></div>
-            <div id=clicker2></div>
-        `;
-    renderClicker1();
-    renderClicker2();
+        <div>Daniel's Clickers</div>
+        <div id=clickerA></div>
+        <div id=clickerB></div>
+    `;
+    clickerA();
+    clickerB();
   }
 
-  return renderApp;
+  return render;
 }
 
-const renderApp = app("root");
+const app = App("root");
 
 function render() {
   console.log("render");
-  renderApp();
+  app();
 }
 
 render();
