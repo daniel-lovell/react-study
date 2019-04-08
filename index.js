699436475d221fd8567fb4b1bd1fff44ea5c8291
Daniel Lovell <danielalovell@gmail.com>
2019-04-07 19:31:20 -0700

    add lifted state and callbacks as props

--- index.js
+++ index.js
@@ -1,21 +1,98 @@
-function Clicker(mountPoint, props) {
-  console.log("Clicker" + props.id);
+// Todo: Lists, Callback Props
+
+function ClickerLifted(mountPoint, props) {
+  console.log("ClickerLifted" + props.id);
+
+  function render(props) {
+    console.log("clickerLifted" + props.id);
+
+    function handleClick() {
+      props.handleClick(props.id);
+    }
+    document.getElementById(mountPoint).innerHTML = `
+        <div>Clicker ${props.id}</div>
+        <div>Clicks: ${props.count}</div>
+        <button id=${props.id}>Click Me!</button>
+    `;
+    document.getElementById(props.id).addEventListener("click", handleClick);
+    // if (state.eventElement) {
+    //   document.getElementById(state.eventElement).focus();
+    // }
+  }
+
+  return render;
+}
+
+function ClickerLiftedApp(mountPoint) {
+  console.log("ClickerLiftedApp");
+  const state = {
+    count: { A: 0, B: 0 }
+  };
+
+  function setState(state, newState) {
+    Object.assign(state, newState);
+    render();
+  }
+
+  function handleClick(id) {
+    setState(state.count, { [id]: state.count[id] + 1 });
+  }
+
+  const clickerLiftedA = ClickerLifted("clickerLiftedA", {
+    id: "A"
+    // count: state.count.A,
+    // handleClick: handleClick
+  });
+  const clickerLiftedB = ClickerLifted("clickerLiftedB", {
+    id: "B"
+    // count: state.count.B,
+    // handleClick: handleClick
+  });
+
+  function render(props) {
+    console.log("clickerLiftedApp");
+    document.getElementById(mountPoint).innerHTML = `
+        <div>Clickers - Lifted State - Total: <span>${state.count.A +
+          state.count.B}</span></div>
+        <div id=clickerLiftedA></div>
+        <div id=clickerLiftedB></div>
+    `;
+    clickerLiftedA({
+      id: "A",
+      count: state.count.A,
+      handleClick: handleClick
+    });
+    clickerLiftedB({
+      id: "B",
+      count: state.count.B,
+      handleClick: handleClick
+    });
+  }
+
+  return render;
+}
+
+function ClickerIndividual(mountPoint, props) {
+  console.log("ClickerIndividual" + props.id);
   const state = {
     count: 0,
     eventElement: null
   };
 
-  function setState(newState) {
+  function setState(state, newState, props) {
     Object.assign(state, newState);
-    render();
+    render(props);
   }
 
-  function handleClick(e) {
-    setState({ count: state.count + 1, eventElement: e.target.id });
-  }
-
-  function render() {
-    console.log("clicker" + props.id);
+  function render(props) {
+    console.log("clickerIndividual" + props.id);
+    function handleClick(e) {
+      setState(
+        state,
+        { count: state.count + 1, eventElement: e.target.id },
+        props
+      );
+    }
     document.getElementById(mountPoint).innerHTML = `
         <div>Clicker ${props.id}</div>
         <div>Clicks: ${state.count}</div>
@@ -30,21 +107,49 @@ function Clicker(mountPoint, props) {
   return render;
 }
 
+function ClickerIndividualApp(mountPoint) {
+  console.log("ClickerIndividualApp");
+
+  const clickerIndividualA = ClickerIndividual("clickerIndividualA", {
+    id: "individualA"
+  });
+  const clickerIndividualB = ClickerIndividual("clickerIndividualB", {
+    id: "individualB"
+  });
+
+  function render(props) {
+    console.log("clickerIndividualApp");
+    document.getElementById(mountPoint).innerHTML = `
+        <div>Clickers - Individual State</div>
+        <div id=clickerIndividualA></div>
+        <div id=clickerIndividualB></div>
+    `;
+    clickerIndividualA({
+      id: "individualA"
+    });
+    clickerIndividualB({
+      id: "individualB"
+    });
+  }
+
+  return render;
+}
+
 function App(mountPoint) {
   console.log("App");
 
-  const clickerA = Clicker("clickerA", { id: "A" });
-  const clickerB = Clicker("clickerB", { id: "B" });
+  const clickerIndividualApp = ClickerIndividualApp("clickerIndividualApp");
+  const clickerLiftedApp = ClickerLiftedApp("clickerLiftedApp");
 
   function render() {
     console.log("app");
     document.getElementById(mountPoint).innerHTML = `
-        <div>Daniel's Clickers</div>
-        <div id=clickerA></div>
-        <div id=clickerB></div>
+      <div id="clickerIndividualApp"></div>
+      <hr />
+      <div id="clickerLiftedApp"></div>
     `;
-    clickerA();
-    clickerB();
+    clickerIndividualApp();
+    clickerLiftedApp();
   }
 
   return render;
